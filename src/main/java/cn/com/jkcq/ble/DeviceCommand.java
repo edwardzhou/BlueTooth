268b23db0c3ebb9c86ff60e80432c92ea27a84ae
Edward Zhou <edward.zhq@gmail.com>
2017-07-06 18:56:35 +0800

    DeviceCommand添加属性

--- src/main/java/cn/com/jkcq/ble/DeviceCommand.java
+++ src/main/java/cn/com/jkcq/ble/DeviceCommand.java
@@ -1,7 +1,26 @@
 package cn.com.jkcq.ble;
 
+import java.util.UUID;
+
 /**
  * Created by edwardzhou on 2017/7/3.
  */
 public interface DeviceCommand {
+    /**
+     * 获取命令名称
+     * @return String
+     */
+    String getCommandName();
+
+    /**
+     * 获取命令对应的特征通道
+     * @return
+     */
+    UUID getUUID();
+
+    /**
+     * 指令字节数据
+     * @return
+     */
+    byte[] getData();
 }
